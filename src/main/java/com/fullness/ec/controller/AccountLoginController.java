d6e58b2e10ce3acb071cb3855ad8bfd09c1e058a
Ryohei Furuichi <r-furuichi@nstec.jp>
2024-07-24 13:01:12 +0900

    formのクラス図修正

--- src/main/java/com/fullness/ec/controller/AccountLoginController.java
+++ src/main/java/com/fullness/ec/controller/AccountLoginController.java
@@ -1,6 +1,7 @@
 package com.fullness.ec.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -8,31 +9,23 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.fullness.ec.form.FormEmployeeLogin;
-import com.fullness.ec.form.FormEmployeeLoginValidator;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.fullness.ec.service.EmployeeService;
 
-@RestController
+@Controller
 @RequestMapping
 public class AccountLoginController {
-    @Autowired FormEmployeeLoginValidator validator;
-    @InitBinder("formEmployeeLogin")
-    public void initBinder(WebDataBinder binder){
-        binder.addValidators(validator);
-    }
+
     @Autowired EmployeeService employeeService;
-    @GetMapping("login")
+    @GetMapping("loginEmployee")
     public String form(){
         return "login";
     }
 
     @GetMapping("menu")
-    public String login(@Validated @ModelAttribute("formEmployeeLogin")FormEmployeeLogin formEmployeeLogin, BindingResult bindingResult, Model model){
-        return "redirect:/menu";
+    public String login(){
+        return "menu";
     }
 }
