c66d659458c5711b1f41fa4c3b109b4d7486e765
Hiromu Yamasaki <h-yamasaki@nstec.jp>
2024-07-25 16:00:43 +0900

    商品検索画面　プルダウン実装、ページネーションの実装途中

--- src/main/java/com/fullness/ec/controller/SearchProductController.java
+++ src/main/java/com/fullness/ec/controller/SearchProductController.java
@@ -8,11 +8,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.fullness.ec.entity.Product;
 import com.fullness.ec.entity.ProductCategory;
+import com.fullness.ec.form.CategoryForm;
 import com.fullness.ec.service.ProductCategoryService;
 import com.fullness.ec.service.ProductService;
 
@@ -21,18 +23,19 @@ import com.fullness.ec.service.ProductService;
 @RequestMapping("product")
 @SessionAttributes("categoryList")
 public class SearchProductController {
+    @ModelAttribute("categoryForm")
+    public CategoryForm setUpForm(){return new CategoryForm();}
     @Autowired ProductService service;
     @Autowired ProductCategoryService categoryService;
 
     @GetMapping("productlist")
     public String searchList(@PageableDefault(page=0,size=5)Pageable pageable, Model model) {
-        // Page<Product> products = service.selectProductByPage(pageable);
-        // model.addAttribute("pageUrl", "/product/productlist?");
-        // model.addAttribute("products",products); 
-        // model.addAttribute("next",pageable.getPageNumber()+2);
-        // model.addAttribute("prev",pageable.getPageNumber());
+        Page<Product> products = service.selectProductByPage(pageable);
+        model.addAttribute("pageUrl", "/product/productlist?");
+        model.addAttribute("products",products); 
+        model.addAttribute("next",pageable.getPageNumber()+2);
+        model.addAttribute("prev",pageable.getPageNumber());
         model.addAttribute("categoryList", categoryService.selectAll());
         return "product/productlist";
     }
-    
 }
