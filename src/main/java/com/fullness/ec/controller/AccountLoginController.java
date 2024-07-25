687f20cdb6cf46597adf563ffe10f30fab6c0830
Ryohei Furuichi <r-furuichi@nstec.jp>
2024-07-25 10:24:32 +0900

    微調整

--- src/main/java/com/fullness/ec/controller/AccountLoginController.java
+++ src/main/java/com/fullness/ec/controller/AccountLoginController.java
@@ -1,16 +1,11 @@
 package com.fullness.ec.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fullness.ec.service.EmployeeService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping
