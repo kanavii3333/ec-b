079ea0c60112a18015d6126d801c1ba9802a929d
kanae <k-tatekawa@nstec.jp>
2024-07-26 14:09:46 +0900

    EmployeeService.java EmployeeServiceImpl.java

--- src/main/java/com/fullness/ec/service/EmployeeServiceImpl.java
+++ src/main/java/com/fullness/ec/service/EmployeeServiceImpl.java
@@ -1,33 +1,32 @@
-// package com.fullness.ec.service;
+package com.fullness.ec.service;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.fullness.ec.entity.EmployeeAccount;
-// import com.fullness.ec.entity.Product;
-// import com.fullness.ec.form.EmployeeForm;
-// import com.fullness.ec.helper.EmployeeConverter;
-// import com.fullness.ec.helper.ProductConverter;
-// import com.fullness.ec.repository.EmployeeRepository;
+import com.fullness.ec.entity.Employee;
+import com.fullness.ec.entity.EmployeeAccount;
+import com.fullness.ec.form.EmployeeForm;
+import com.fullness.ec.helper.EmployeeConverter;
 
-// @Service
-// @Transactional
-// public class EmployeeServiceImpl implements EmployeeService {
-// @Autowired
-// private EmployeeRepository employeeRepository;
+import com.fullness.ec.repository.EmployeeRepository;
 
-// @Override
-// public void addAccount(EmployeeForm EmployeeForm) {
-// EmployeeAccount employeeAccount =
-// EmployeeConverter.converterFormEmpAccountToEmployee(emoloyeeForm);
-// employeeRepository.insert(emoloyeeForm);
-// }
+@Service
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-// @Override
-// public List<EmployeeForm> getEmployeeList() {
-// return employeeRepository.selectAllEmployee();
-// }
-// }
+    @Override
+    public void addAccount(EmployeeForm employeeForm) {
+        EmployeeAccount employeeAccount = EmployeeConverter.converterFormEmpAccountToEmployee(employeeForm);
+        employeeRepository.insert(employeeAccount);
+    }
+
+    @Override
+    public List<Employee> getEmployeeList() {
+        return employeeRepository.selectAllEmployee();
+    }
+}
