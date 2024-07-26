1ac63bcddd34509c646dfff46098b9c535cbd0dc
kanae <k-tatekawa@nstec.jp>
2024-07-26 13:13:23 +0900

    クラス図の修正

--- src/main/java/com/fullness/ec/service/EmployeeService.java
+++ src/main/java/com/fullness/ec/service/EmployeeService.java
@@ -6,7 +6,5 @@ import java.util.List;
 public interface EmployeeService {
     void addAccount(EmployeeForm EmployeeForm);
 
-    boolean employeeLogin(String name, String password);
-
     List<EmployeeForm> getEmployeeList();
 }
