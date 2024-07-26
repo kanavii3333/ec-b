61e042fb0faa3230d4e3ddea9ccd96c105cc2b9d
kanae <k-tatekawa@nstec.jp>
2024-07-26 12:32:58 +0900

    EmployeeService.java インターフェース

--- src/main/java/com/fullness/ec/service/EmployeeService.java
+++ src/main/java/com/fullness/ec/service/EmployeeService.java
@@ -1,14 +1,12 @@
 package com.fullness.ec.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.fullness.ec.form.EmployeeForm;
+import java.util.List;
 
-import com.fullness.ec.repository.EmployeeRepository;
+public interface EmployeeService {
+    void addAccount(EmployeeForm EmployeeForm);
 
-@Service
-@Transactional
-public class EmployeeService {
-    @Autowired EmployeeRepository employeeRepository;
-    
+    boolean employeeLogin(String name, String password);
+
+    List<EmployeeForm> getEmployeeList();
 }
