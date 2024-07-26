fb6d219a3bb7723317aaba41c5ddba640b2df738
kanae <k-tatekawa@nstec.jp>
2024-07-26 15:34:26 +0900

    AddAccountController.java

--- src/main/java/com/fullness/ec/controller/AddAccountController.java
+++ src/main/java/com/fullness/ec/controller/AddAccountController.java
@@ -1,86 +1,84 @@
-// package com.fullness.ec.controller;
+package com.fullness.ec.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
-// import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.SessionAttributes;
-// import org.springframework.web.bind.support.SessionStatus;
-// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-// import com.fullness.ec.entity.Employee;
+import com.fullness.ec.entity.Employee;
 
-// import com.fullness.ec.form.EmployeeForm;
+import com.fullness.ec.form.EmployeeForm;
 
-// import com.fullness.ec.service.EmployeeService;
+import com.fullness.ec.service.EmployeeService;
 
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-// @SessionAttributes({ "employeeForm", "employeeList" })
-// @RequestMapping("registeraccount")
-// @Controller
-// public class AddAccountController {
+@SessionAttributes({ "employeeForm", "employeeList" })
+@RequestMapping("registeraccount")
+@Controller
+public class AddAccountController {
 
-// @ModelAttribute("employeeForm")
-// public EmployeeForm setupForm() {
-// return new EmployeeForm();
-// }
+    @ModelAttribute("employeeForm")
+    public EmployeeForm setupForm() {
+        return new EmployeeForm();
+    }
 
-// // @Autowired
-// // private EmployeeForm employeeForm;
+    // @Autowired
+    // private EmployeeForm employeeForm;
 
-// @Autowired
-// private EmployeeService employeeService;
+    @Autowired
+    private EmployeeService employeeService;
 
-// @GetMapping("input")
-// public String input(Model model) {
+    @GetMapping("input")
+    public String input(Model model) {
 
-// model.addAttribute("employeeList", employeeService.getEmployeeList());
-// return "registeraccount/input";
-// }
+        model.addAttribute("employeeList", employeeService.getEmployeeList());
+        return "registeraccount/input";
+    }
 
-// @PostMapping("confirm")
-// public String comfirm(
-// @ModelAttribute("employeeForm") EmployeeForm employeeForm,
-// BindingResult bindingResult, Model model) {
-// System.out.println("employeeForm:" + employeeForm);
-// System.out.println("bindingResult:" + bindingResult);
-// if (bindingResult.hasErrors())
-// return "registeraccount/input";
+    @PostMapping("confirm")
+    public String comfirm(
+            @ModelAttribute("employeeForm") EmployeeForm employeeForm,
+            BindingResult bindingResult, Model model) {
+        System.out.println("employeeForm:" + employeeForm);
+        System.out.println("bindingResult:" + bindingResult);
+        if (bindingResult.hasErrors())
+            return "registeraccount/input";
 
-// Employee employee = null;
-// for (Employee emp : employeeService.getEmployeeList()) {
-// if (emp.getEmpId().equals(employeeForm.getId())) {
-// employee = emp;
-// break;
-// }
+        Employee employee = null;
+        for (Employee emp : employeeService.getEmployeeList()) {
+            if (emp.getEmpId().equals(employeeForm.getId())) {
+                employee = emp;
+                break;
+            }
 
-// }
+        }
 
-// model.addAttribute("employee", employee);
-// model.addAttribute("employeeForm", employeeForm);
-// return "registeraccount/confirm";
-// }
+        model.addAttribute("employee", employee);
+        model.addAttribute("employeeForm", employeeForm);
+        return "registeraccount/confirm";
+    }
 
-// @PostMapping("execute")
-// public String execute(@ModelAttribute("employeeForm") EmployeeForm
-// employeeForm,
-// RedirectAttributes redirectAttributes) {
+    @PostMapping("execute")
+    public String execute(@ModelAttribute("employeeForm") EmployeeForm employeeForm,
+            RedirectAttributes redirectAttributes) {
 
-// redirectAttributes.addFlashAttribute("employeeForm", employeeForm);
-// return "redirect:/registeraccount/complete";
-// }
+        redirectAttributes.addFlashAttribute("employeeForm", employeeForm);
+        return "redirect:/registeraccount/complete";
+    }
 
-// @GetMapping("complete")
+    @GetMapping("complete")
 
-// public String complete(@ModelAttribute("employeeForm") EmployeeForm
-// employeeForm, SessionStatus sessionStatus) {
-// System.out.println("employeeForm:" + employeeForm);
-// sessionStatus.setComplete();
-// return "registeraccount/complete";
-// }
-// }
+    public String complete(@ModelAttribute("employeeForm") EmployeeForm employeeForm, SessionStatus sessionStatus) {
+        System.out.println("employeeForm:" + employeeForm);
+        sessionStatus.setComplete();
+        return "registeraccount/complete";
+    }
+}
