c96b3e49d4ebda047f38f2c4548afeed485821d3
Ryohei Furuichi <r-furuichi@nstec.jp>
2024-07-26 09:11:58 +0900

    クラス名変更

--- src/main/java/com/fullness/ec/security/EmployeeUserDetailsService.java
+++ src/main/java/com/fullness/ec/security/EmployeeUserDetailsService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.fullness.ec.entity.EmpAccount;
+import com.fullness.ec.entity.EmployeeAccount;
 import com.fullness.ec.repository.EmployeeRepository;
 
 @Service
@@ -15,7 +15,7 @@ public class EmployeeUserDetailsService implements UserDetailsService {
     @Autowired EmployeeRepository repository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        EmpAccount employee = repository.selectByUsername(username);
+        EmployeeAccount employee = repository.selectByUsername(username);
         if(employee==null) throw new UsernameNotFoundException("ユーザーが存在しません");
         return new EmployeeUserDetails(employee, AuthorityUtils.createAuthorityList("Employee")) {
         };
