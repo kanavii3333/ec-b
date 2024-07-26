2392962d7afcb8bf4d43dda77065456d1afae61b
Ryohei Furuichi <r-furuichi@nstec.jp>
2024-07-26 14:35:08 +0900

    商品の登録はできた

--- src/main/java/com/fullness/ec/service/ProductServiceImpl.java
+++ src/main/java/com/fullness/ec/service/ProductServiceImpl.java
@@ -8,20 +8,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fullness.ec.entity.Product;
 import com.fullness.ec.entity.ProductCategory;
+import com.fullness.ec.entity.ProductStock;
 import com.fullness.ec.form.ProductForm;
 import com.fullness.ec.helper.ProductConverter;
 import com.fullness.ec.repository.ProductCategoryRepository;
 import com.fullness.ec.repository.ProductRepository;
+import com.fullness.ec.repository.StockRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
     @Autowired
-    private ProductCategoryRepository productCategoryRepository;
+    private StockRepository stockRepository;
 
     @Override
     public List<Product> getProductList() {
@@ -45,9 +48,12 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     public void addProduct(ProductForm productForm, byte[] imageByte) {
         Product product = ProductConverter.convertToEntity(productForm, imageByte);
         productRepository.insert(product);
+        product.getProductStock().setProductId(productRepository.selectByName(product.getProductName()).getProductId());
+        //stockRepository.insert(product.getProductStock());
     }
 
     @Override
