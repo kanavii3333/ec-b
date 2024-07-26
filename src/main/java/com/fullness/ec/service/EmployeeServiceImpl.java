6d441fb1e4255272d5be12435e778893f307d137
kanae <k-tatekawa@nstec.jp>
2024-07-26 12:54:26 +0900

    ProuductService.java インターフェース

---
+++ src/main/java/com/fullness/ec/service/EmployeeServiceImpl.java
@@ -0,0 +1,17 @@
+// package com.fullness.ec.service;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
+
+// import com.fullness.ec.repository.EmployeeRepository;
+
+// @Service
+// @Transactional
+// public class EmployeeServiceImpl implements EmployeeService {
+// @Autowired
+// private EmployeeRepository employeeRepository;
+
+// @Override
+
+// }
