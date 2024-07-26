7daba2c87a840c75998dd3527a6bd8b8bc084241
kanae <k-tatekawa@nstec.jp>
2024-07-26 11:28:54 +0900

    Employee.java

--- src/main/java/com/fullness/ec/entity/Employee.java
+++ src/main/java/com/fullness/ec/entity/Employee.java
@@ -4,5 +4,9 @@ import lombok.Data;
 
 @Data
 public class Employee {
-    
+    private Integer empId;
+    private String empName;
+    private String empNameKana;
+    private Department department;
+    private EmployeeAccount empAccount;
 }
