71f54d9012d730819dc26a51899105ffd1f06606
kanae <k-tatekawa@nstec.jp>
2024-07-29 10:41:39 +0900

    ProductCategoryService　修正

--- src/main/java/com/fullness/ec/service/ProductCategoryService.java
+++ src/main/java/com/fullness/ec/service/ProductCategoryService.java
@@ -1,10 +1,11 @@
 package com.fullness.ec.service;
 
+import com.fullness.ec.entity.ProductCategory;
 import com.fullness.ec.form.CategoryForm;
 import java.util.List;
 
 public interface ProductCategoryService {
     void addCategory(CategoryForm categoryForm);
 
-    List<CategoryForm> selectAll();
+    List<ProductCategory> selectAll();
 }
--- src/main/java/com/fullness/ec/service/ProductCategoryServiceImpl.java
+++ src/main/java/com/fullness/ec/service/ProductCategoryServiceImpl.java
@@ -1,18 +1,32 @@
 package com.fullness.ec.service;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fullness.ec.entity.EmployeeAccount;
 import com.fullness.ec.entity.ProductCategory;
+import com.fullness.ec.form.CategoryForm;
+import com.fullness.ec.form.EmployeeForm;
+import com.fullness.ec.helper.EmployeeConverter;
+import com.fullness.ec.helper.ProductConverter;
 import com.fullness.ec.repository.ProductCategoryRepository;
 
 @Service
-public class ProductCategoryServiceImpl {
+public class ProductCategoryServiceImpl implements ProductCategoryService {
     @Autowired
     ProductCategoryRepository productCategoryRepository;
 
+    @Override
+    public void addCategory(CategoryForm categoryForm) {
+
+        ProductCategory productCategory = ProductConverter.convertFormCategoryToEntity(categoryForm);
+        productCategoryRepository.insert(productCategory);
+    }
+
+    @Override
     public List<ProductCategory> selectAll() {
         return productCategoryRepository.selectAll();
     }
