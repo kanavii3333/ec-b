b4d43026b8c49b2623db3ef7ddf9703f71e0f28d
kanae <k-tatekawa@nstec.jp>
2024-07-29 10:58:10 +0900

    RegisterCategoryProductController.java

--- src/main/java/com/fullness/ec/controller/RegisterCategoryProductController.java
+++ src/main/java/com/fullness/ec/controller/RegisterCategoryProductController.java
@@ -1,63 +1,60 @@
-// package com.fullness.ec.controller;
+package com.fullness.ec.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.validation.BindingResult;
-// import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.SessionAttributes;
-// import org.springframework.web.bind.support.SessionStatus;
-// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-// import com.fullness.ec.form.CategoryForm;
-// import com.fullness.ec.service.ProductCategoryServiceImpl;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
+import com.fullness.ec.form.CategoryForm;
+import com.fullness.ec.service.ProductCategoryServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-// @SessionAttributes("categoryForm")
-// @RequestMapping("registerproduct")
-// @Controller
-// public class RegisterCategoryProductController {
-// @ModelAttribute("categoryForm")
-// public CategoryForm setupForm() {
-// return new CategoryForm();
-// }
+@SessionAttributes("categoryForm")
+@RequestMapping("registerproduct")
+@Controller
+public class RegisterCategoryProductController {
+    @ModelAttribute("categoryForm")
+    public CategoryForm setupForm() {
+        return new CategoryForm();
+    }
 
-// @Autowired
-// ProductCategoryServiceImpl productCategoryServiceImpl;
+    @Autowired
+    ProductCategoryServiceImpl productCategoryServiceImpl;
 
-// @GetMapping("input")
-// public String input() {
-// return "product/categoryregister/input";
-// }
+    @GetMapping("input")
+    public String input() {
+        return "product/categoryregister/input";
+    }
 
-// @PostMapping("confirm")
-// public String confirm(@ModelAttribute("categoryForm") CategoryForm
-// categoryForm, BindingResult bindingResult,
-// Model model) {
-// if (bindingResult.hasErrors())
-// return "product/categoryregister/input";
-// model.addAttribute("categoryForm", categoryForm);
-// return "product/categoryregister/confirm";
-// }
+    @PostMapping("confirm")
+    public String confirm(@ModelAttribute("categoryForm") CategoryForm categoryForm, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors())
+            return "product/categoryregister/input";
+        model.addAttribute("categoryForm", categoryForm);
+        return "product/categoryregister/confirm";
+    }
 
-// @PostMapping("execute")
-// public String execute(@ModelAttribute("categoryForm") CategoryForm
-// categoryForm,
-// RedirectAttributes redirectAttributes) {
+    @PostMapping("execute")
+    public String execute(@ModelAttribute("categoryForm") CategoryForm categoryForm,
+            RedirectAttributes redirectAttributes) {
 
-// redirectAttributes.addFlashAttribute("categoryForm", categoryForm);
+        redirectAttributes.addFlashAttribute("categoryForm", categoryForm);
 
-// productCategoryServiceImpl.addCategory(categoryForm);
-// return "redirect:/product/categoryregister/complete";
-// }
+        productCategoryServiceImpl.addCategory(categoryForm);
+        return "redirect:/product/categoryregister/complete";
+    }
 
-// @GetMapping("complete")
-// public String complete(@ModelAttribute("categoryForm") CategoryForm
-// categoryForm, SessionStatus sessionStatus) {
-// sessionStatus.setComplete();
-// return "product/categoryregister/complete";
-// }
+    @GetMapping("complete")
+    public String complete(@ModelAttribute("categoryForm") CategoryForm categoryForm, SessionStatus sessionStatus) {
+        sessionStatus.setComplete();
+        return "product/categoryregister/complete";
+    }
 
-// }
+}
