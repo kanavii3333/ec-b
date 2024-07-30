938498ca99d7d0c403bdaac1832e88e8311daf85
Ryohei Furuichi <r-furuichi@nstec.jp>
2024-07-30 11:14:25 +0900

    不要importの削除

--- src/main/java/com/fullness/ec/controller/AccountLoginController.java
+++ src/main/java/com/fullness/ec/controller/AccountLoginController.java
@@ -1,11 +1,9 @@
 package com.fullness.ec.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fullness.ec.service.EmployeeService;
 
 @Controller
 @RequestMapping
