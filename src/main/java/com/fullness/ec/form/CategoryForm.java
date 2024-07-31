91ffdf13620a3bc9632a1d3e014c9ec61ce6c68d
Hiromu Yamasaki <h-yamasaki@nstec.jp>
2024-07-31 10:48:27 +0900

    商品カテゴリ登録のエラーメッセージ修正

--- src/main/java/com/fullness/ec/form/CategoryForm.java
+++ src/main/java/com/fullness/ec/form/CategoryForm.java
@@ -13,7 +13,7 @@ public class CategoryForm {
     
     private Integer productCategoryId;
     @NotBlank
-    @Length(min=2,max=20)
-    @Pattern(regexp = "^[a-zA-Z0-9\\u4E00-\\u9FAF\\u3040-\\u309F\\u30A0-\\u30FF]+$", message = "{pattern.name}")
+    @Length(min=2,max=20, message = "{length.productCategoryName}")
+    @Pattern(regexp = "^[a-zA-Z0-9\\u4E00-\\u9FAF\\u3040-\\u309F\\u30A0-\\u30FF]+$", message = "{pattern.productCategoryName}")
     private String productCategoryName; 
 }
