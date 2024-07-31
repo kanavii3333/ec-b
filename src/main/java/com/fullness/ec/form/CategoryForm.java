c8d205ea0efbfade62c547cc1e963e311aaeb458
Hiromu Yamasaki <h-yamasaki@nstec.jp>
2024-07-31 10:19:31 +0900

    バリデーション修正

--- src/main/java/com/fullness/ec/form/CategoryForm.java
+++ src/main/java/com/fullness/ec/form/CategoryForm.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -13,5 +14,6 @@ public class CategoryForm {
     private Integer productCategoryId;
     @NotBlank
     @Length(min=2,max=20)
+    @Pattern(regexp = "^[a-zA-Z0-9\\u4E00-\\u9FAF\\u3040-\\u309F\\u30A0-\\u30FF]+$", message = "{pattern.name}")
     private String productCategoryName; 
 }
--- src/main/java/com/fullness/ec/form/EmployeeForm.java
+++ src/main/java/com/fullness/ec/form/EmployeeForm.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -17,10 +18,12 @@ public class EmployeeForm {
 
     @NotBlank
     @Length(min=5,max=20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern.empAccountName}")
     private String empAccountName;
 
     @NotBlank
     @Length(min=5,max=20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern.empAccountPassword}")
     private String empAccountPassword;
 
 }
