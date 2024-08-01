3f3eefae064581f55fade434269c41939fde78d3
Hiromu Yamasaki <h-yamasaki@nstec.jp>
2024-08-01 12:57:27 +0900

    ログインのエラーメッセージ修正、追加したメソッドのテスト追加

--- src/main/java/com/fullness/ec/security/SecurityConfig.java
+++ src/main/java/com/fullness/ec/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,14 +28,19 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder encoder;
 
-    @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoderencoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(service);
-        authenticationProvider.setPasswordEncoder(encoder);
-        return new ProviderManager(authenticationProvider);
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(service).passwordEncoder(encoder);
     }
+
+//     @Bean
+//     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+//             PasswordEncoder passwordEncoderencoder) {
+//         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//         authenticationProvider.setUserDetailsService(service);
+//         authenticationProvider.setPasswordEncoder(encoder);
+//         return new ProviderManager(authenticationProvider);
+//     }
 
     @Bean
     public WebSecurityCustomizer webCustomizer() {
@@ -47,15 +53,29 @@ public class SecurityConfig {
         // http.formLogin(login->login.loginProcessingUrl("/auth_customer").loginPage("/loginCustomer")
         // .usernameParameter("mailAddress").passwordParameter("password").defaultSuccessUrl("/menu").failureUrl("/login").permitAll());
 
-        http.formLogin(login -> login.loginProcessingUrl("/auth_employee").loginPage("/loginEmployee")
-                .usernameParameter("username").passwordParameter("password").defaultSuccessUrl("/menu", true)// ここにtrueを付けました。
-                .failureUrl("/loginEmployee?error=true").permitAll());// ここに?error=trueを付けました
+        http.formLogin(login -> login
+                .loginProcessingUrl("/auth_employee")
+                .loginPage("/loginEmployee")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/menu", true)// ここにtrueを付けました。
+                .failureUrl("/loginEmployee?error=true")// ここに?error=trueを付けました
+                .permitAll());
 
-        http.logout(logout -> logout.logoutUrl("/logoutEmployee").logoutSuccessUrl("/menu").invalidateHttpSession(true)
-                .deleteCookies("JSESSONID").clearAuthentication(true).permitAll());
+        http.logout(logout -> logout
+                .logoutUrl("/logoutEmployee")
+                .logoutSuccessUrl("/menu")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSONID")
+                .clearAuthentication(true)
+                .permitAll());
 
-        http.authorizeHttpRequests(authz -> authz.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        http.authorizeHttpRequests(authz -> authz
+                .dispatcherTypeMatchers(DispatcherType.ERROR)
+                .permitAll()
+                .requestMatchers(PathRequest.toStaticResources()
+                .atCommonLocations())
+                .permitAll()
                 // .requestMatchers("/**").permitAll() //全てセキュリティオフ
                 .requestMatchers("/login", "/menu", "/img/**").permitAll()
                 .requestMatchers("/logout", "/registerproduct/**", "/deleteproduct/**", "/updateproduct/**",
--- src/test/java/com/fullness/ec/service/ProductServiceTest.java
+++ src/test/java/com/fullness/ec/service/ProductServiceTest.java
@@ -1,16 +1,13 @@
 package com.fullness.ec.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -96,11 +93,18 @@ public class ProductServiceTest {
     
     @Sql("/sql/data.sql")
     @Test
-    public void testSelectProductByPageWithProducts() {
+    public void selectProductByPageWithProductsTest() {
         // Arrange
         Pageable pageable = PageRequest.of(1, 5);
 
         assertEquals(new PageImpl<>(productRepository.selectByPage(pageable, null),pageable,25), productService.selectProductByPage(pageable, null));
 
+    }
+    @Sql("/sql/data.sql")
+    @Test
+    public void isProductExistTest() {
+        ProductForm productForm = new ProductForm();
+        productForm.setProductName("水性ボールペン(黒)");
+        assertEquals(true, productService.isProductExist(productForm));
     }
 }
