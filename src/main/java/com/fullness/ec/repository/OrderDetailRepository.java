ffa459407dd138ed1bea1ee31b3e805dba1d4374
Hiromu Yamasaki <h-yamasaki@nstec.jp>
2024-08-02 17:07:56 +0900

    フロントで使うRepositoryクラスの追加（中身コメントアウト）

---
+++ src/main/java/com/fullness/ec/repository/OrderDetailRepository.java
@@ -0,0 +1,8 @@
+package com.fullness.ec.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDetailRepository {
+   // void insert(OrderDetail orderDetail);
+}
