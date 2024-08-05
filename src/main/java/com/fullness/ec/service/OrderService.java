34473def31cb8f08fb6aa2e01acb6ecfda7d0735
kanae <k-tatekawa@nstec.jp>
2024-08-05 11:13:53 +0900

    OrderService.java

---
+++ src/main/java/com/fullness/ec/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.fullness.ec.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+
+import com.fullness.ec.entity.Order;
+import com.fullness.ec.entity.OrderStatus;
+import com.fullness.ec.entity.PaymentMethod;
+import com.fullness.ec.entity.ProductStock;
+import com.fullness.ec.form.OrderDetailForm;
+import com.fullness.ec.form.OrderForm;
+import com.fullness.ec.repository.OrderStatusRepository;
+
+public interface OrderService {
+
+    void registerOrder(List<OrderDetailForm> orderDetailForm, OrderForm orderForm);
+
+    void deleteOrder();
+
+    void updateStatus(OrderForm orderForm);
+
+    // List<OrderStatusRepository> getOrderStatusList();
+
+    List<Order> getOrderList(String mailAddress);
+
+    ProductStock getStockByProductId(Integer productId);
+
+    // ProductStock OrderDetailForm(OrderDetailForm orderDetailForm);
+
+    List<PaymentMethod> getPaymentMethodList();
+
+    // List<OrderStatus> getOrderStatusList();
+
+    // Page<Order> selectPurchaseHistoryByPage(Pageable pageable, LocalDateTime
+    // date, Integer customerId);
+
+    void getStockByOrderDetailForm(OrderDetailForm orderDetailForm);
+
+}
