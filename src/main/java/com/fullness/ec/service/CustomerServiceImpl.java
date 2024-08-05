12fa53d17a7fa8b7afa63b40e978f23521c67e10
Kirari Takara <k-takara@nstec.jp>
2024-08-05 09:22:19 +0900

    CustomerService ProductService作成

---
+++ src/main/java/com/fullness/ec/service/CustomerServiceImpl.java
@@ -0,0 +1,43 @@
+package com.fullness.ec.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fullness.ec.entity.Customer;
+import com.fullness.ec.entity.EmployeeAccount;
+import com.fullness.ec.form.CustomerForm;
+import com.fullness.ec.helper.EmployeeConverter;
+import com.fullness.ec.repository.CustomerRepository;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService{
+    @Autowired 
+    private CustomerRepository customerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public List<Customer> selectAll(){
+        return customerRepository.selectAll();
+    };
+
+    @Override
+    public Customer getCustomerByMailAddress(String address){
+        Customer customer = customerRepository.selectByMailAddress(address);
+        return customer;
+
+    };
+
+    @Override
+    public void registerCustomer(CustomerForm customer){
+        //    CustomerAccount customerAccount = CustomerConverter.converterCustomerFormAccountToEmployee(customer);
+        //    customerAccount.setPassword(passwordEncoder.encode(customerAccount.getPassword()));
+        //    customerRepository.insert(customerAccount);
+    };
+    
+}
