7577b2950275f401e125594412d8c4cb1c4b0c78
kanae <k-tatekawa@nstec.jp>
2024-08-07 15:01:31 +0900

    ログアウトするための修正　セキュリティコンフィグ触りました

--- src/main/java/com/fullness/ec/security/SecurityConfig.java
+++ src/main/java/com/fullness/ec/security/SecurityConfig.java
@@ -18,15 +18,15 @@ import jakarta.servlet.DispatcherType;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  
+
   @Bean
   public PasswordEncoder encoder() {
-      return new BCryptPasswordEncoder();
+    return new BCryptPasswordEncoder();
   }
 
   @Configuration
   @Order(2)
-  public static class BackendConfigrationAdapter{
+  public static class BackendConfigrationAdapter {
 
     @Autowired
     private EmployeeUserDetailsService backendUserDetailsService;
@@ -34,21 +34,22 @@ public class SecurityConfig {
     private PasswordEncoder encoder;
 
     // @Autowired
-    // public void authenticationManager(AuthenticationManagerBuilder builder, PasswordEncoder encoder) throws Exception {
-    //   builder.userDetailsService(backendUserDetailsService).passwordEncoder(encoder);
+    // public void authenticationManager(AuthenticationManagerBuilder builder,
+    // PasswordEncoder encoder) throws Exception {
+    // builder.userDetailsService(backendUserDetailsService).passwordEncoder(encoder);
     // }
 
     @Bean
     public SecurityFilterChain backendSecurityFilterChain(HttpSecurity http) throws Exception {
       http.securityMatcher("/admin/**")
           .authorizeHttpRequests(authz -> authz
-          .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll() // エラー画面は認証対象外
-          .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 静的リソースは認証対象外
-          .requestMatchers("/admin/login","/admin/menu").permitAll() // ログイン画面は認証対象外
-          .requestMatchers("/admin/**").hasAnyAuthority("ADMIN") // 認証対象
-          .anyRequest()
-          .authenticated() // その他は認証対象
-      );
+              .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll() // エラー画面は認証対象外
+              .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 静的リソースは認証対象外
+              .requestMatchers("/admin/login", "/admin/menu").permitAll() // ログイン画面は認証対象外
+              .requestMatchers("/admin/**").hasAnyAuthority("ADMIN") // 認証対象
+              .anyRequest()
+              .authenticated() // その他は認証対象
+          );
       http.formLogin(login -> login
           .loginProcessingUrl("/admin/auth") // 認証処理を起動するURL
           .loginPage("/admin/login") // ログイン認証画面のURL
@@ -60,25 +61,24 @@ public class SecurityConfig {
       );
       http.logout(logout -> logout
           .logoutUrl("/admin/logout") // ログアウト処理をするURL
-          .logoutSuccessUrl("/admin/login/before") // ログアウト成功時のURL
+          .logoutSuccessUrl("/admin/menu") // ログアウト成功時のURL
           .invalidateHttpSession(true) // ログアウト時はセッションを破棄する
           .deleteCookies("JSESSIONID") // ログアウト時はクッキーを削除する
           .clearAuthentication(true) // ログアウト時は認証情報をクリアする
           .permitAll());
 
       AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-          builder.userDetailsService(backendUserDetailsService).passwordEncoder(encoder);
-          http.authenticationManager(builder.build());
+      builder.userDetailsService(backendUserDetailsService).passwordEncoder(encoder);
+      http.authenticationManager(builder.build());
 
       return http.build();
     }
 
-    
   }
 
   @Configuration
   @Order(1)
-  public static class FrontendConfigrationAdapter{
+  public static class FrontendConfigrationAdapter {
 
     @Autowired
     private CustomerUserDetailsService frontendUserDetailsService;
@@ -87,21 +87,24 @@ public class SecurityConfig {
     private PasswordEncoder encoder;
 
     // @Autowired
-    // public void authenticationManager(AuthenticationManagerBuilder builder, PasswordEncoder encoder) throws Exception {
-    //   builder.userDetailsService(frontendUserDetailsService).passwordEncoder(encoder);
+    // public void authenticationManager(AuthenticationManagerBuilder builder,
+    // PasswordEncoder encoder) throws Exception {
+    // builder.userDetailsService(frontendUserDetailsService).passwordEncoder(encoder);
     // }
 
     @Bean
     public SecurityFilterChain frontendSecurityFilterChain(HttpSecurity http) throws Exception {
       http.securityMatcher("/customer/**")
           .authorizeHttpRequests(authz -> authz
-          .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll() // エラー画面は認証対象外
-          .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 静的リソースは認証対象外
-          .requestMatchers("/customer/login","/customer/top","/customer/productDetails","/customer/searchproduct").permitAll() // ログイン画面は認証対象外
-          .requestMatchers("/customer/**").hasAuthority("CUSTOMER")
-          .anyRequest()
-          .authenticated() // その他は認証対象
-      );
+              .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll() // エラー画面は認証対象外
+              .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 静的リソースは認証対象外
+              .requestMatchers("/customer/login", "/customer/top", "/customer/productDetails",
+                  "/customer/searchproduct")
+              .permitAll() // ログイン画面は認証対象外
+              .requestMatchers("/customer/**").hasAuthority("CUSTOMER")
+              .anyRequest()
+              .authenticated() // その他は認証対象
+          );
       http.formLogin(login -> login
           .loginProcessingUrl("/customer/auth") // 認証処理を起動するURL
           .loginPage("/customer/login") // ログイン認証画面のURL
@@ -120,8 +123,8 @@ public class SecurityConfig {
           .permitAll());
 
       AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-          builder.userDetailsService(frontendUserDetailsService).passwordEncoder(encoder);
-          http.authenticationManager(builder.build());
+      builder.userDetailsService(frontendUserDetailsService).passwordEncoder(encoder);
+      http.authenticationManager(builder.build());
       return http.build();
     }
   }
